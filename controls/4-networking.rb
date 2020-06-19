6bd552bf0cd4cd8b76bd1d04b4fd60ddf137e8c5
Michael Krieg <michael@centrias.de>
2020-06-19 14:58:24 +0200

    check for unrestricted RDP access (port 3389)

--- controls/4-networking.rb
+++ controls/4-networking.rb
@@ -16,3 +16,20 @@ control 'cis-amazon-web-services-foundation-benchmark-4.1' do
     end
   end
 end
+
+control 'cis-amazon-web-services-foundation-benchmark-4.2' do
+  impact 'high'
+  title "Ensure no security groups allow ingress from 0.0.0.0/0 to port 3389"
+  desc "It is recommended that no security group allows unrestricted ingress access to port 3389."
+
+  tag cis: 'CIS_Amazon_Web_Services_Foundations_Benchmark:1.2'
+  tag level: 1
+
+  sg_gids = aws_security_groups.group_ids
+
+  sg_gids.each do |gid|
+    describe aws_security_group(id: gid) do
+      it { should_not allow_in(port: 3389, ipv4_range: '0.0.0.0/0') }
+    end
+  end
+end
