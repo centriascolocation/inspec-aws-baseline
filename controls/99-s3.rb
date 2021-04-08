62b971048ceafb3b05793ba706371b281a30059a
Michael Krieg <michael@centrias.de>
2021-04-08 15:44:07 +0200

    S3 buckets should require requests to use Secure Socket Layer

--- controls/99-s3.rb
+++ controls/99-s3.rb
@@ -3,6 +3,7 @@ title '99 Additional Controls focused on Best Practices - S3'
 control 's3-public-buckets-99.1' do
   title "S3 Public Buckets with Default Encryption enabled"
   desc "All your S3 Buckets should not be public accessible and the AES256 default Encryption should be enabled."
+  ref 'AWS Security Hub, Section S3.5', url: 'https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#s3-5-remediation'
 
   all_buckets = aws_s3_buckets.bucket_names
 
@@ -10,6 +11,9 @@ control 's3-public-buckets-99.1' do
     describe aws_s3_bucket(b) do
       it { should_not be_public }
       it { should have_default_encryption_enabled }
+
+      ## S3 buckets should require requests to use Secure Socket Layer:
+      it { should have_secure_transport_enabled }
     end
   end
 
